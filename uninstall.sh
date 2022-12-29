b8c9fe38d9deb2819effc23c1ec50a724a848df3
Robert Venters <robert@venters.me>
2022-12-29 14:10:24 -0800

    Add SYSD_DIR

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,9 @@
 #! /bin/sh
 
 BIN_DIR=$HOME/.local/bin
+SYSD_DIR=$HOME/.config/systemd/user
 
 echo "Uninstalling Stålenhag Wallpaper service"
 systemctl stop --user stalenhag.service stalenhag.timer
 systemctl disable --user stalenhag.service stalenhag.timer
-sudo rm /etc/systemd/user/stalenhag.*
-rm $BIN_DIR/stalenhag
-rm -rf ~/.stalenhag/
+rm -rf $BIN_DIR/stalenhag $SYSD_DIR/stalenhag.* $HOME/.stalenhag
